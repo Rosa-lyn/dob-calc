5b98a6db37821d234b5a548d87b0ffb39671234a
Rosalyn Land <roz.s.land@gmail.com>
2022-09-30 11:55:57 +0100

    Display selected date from date picker

--- app/src/main/java/com/rosalynland/dobcalc/MainActivity.kt
+++ app/src/main/java/com/rosalynland/dobcalc/MainActivity.kt
@@ -5,35 +5,52 @@ import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
 
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
         btnDatePicker.setOnClickListener {
             clickDatePicker()
         }
     }
 
-    fun clickDatePicker() {
+    private fun clickDatePicker() {
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
         DatePickerDialog(
             this,
             // here we can also remove `DatePickerDialog.OnDateSetListener`
-            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            DatePickerDialog.OnDateSetListener { _, selectedYear, selectedMonth, selectedDayOfMonth ->
                 // this code is executed after clicking ok, but not on cancel
                 Toast.makeText(
                     this,
-                    "DatePicker works",
-                    Toast.LENGTH_SHORT
+                    "dayOfMonth: $selectedDayOfMonth, month: ${selectedMonth + 1}, year: $selectedYear",
+                    Toast.LENGTH_LONG
                 ).show()
+
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+                tvSelectedDate?.text = selectedDate
+
+                // setting the desired date format
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                // converting date into our selected format
+                val convertedDate = sdf.parse(selectedDate)
             },
             year,
             month,
